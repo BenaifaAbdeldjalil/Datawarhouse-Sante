4bb403fa7533d93a6171e8237ffc9fb606a9a7b2
ABDELDJALIL BENAIFA <77646178+BenaifaAbdeldjalil@users.noreply.github.com>
2026-02-15 12:50:46 +0100

    Create 01_init_schemas.sql

---
+++ 01_init_schemas.sql
@@ -0,0 +1,7 @@
+-- 1. Création des schémas
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
+
+-- Optionnel : schéma commun de référentiels
+CREATE SCHEMA IF NOT EXISTS ref;
